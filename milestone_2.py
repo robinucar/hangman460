9666de5841abb945d0ce7abfd698c060ed828a3f
robin ucar <mehmet.ucar@imediapro.co.uk>
2023-12-11 16:49:37 +0000

    created functions

--- milestone_2.py
+++ milestone_2.py
@@ -8,8 +8,8 @@ random_word = random.choice(word_list)
 # Using the input function, ask the user to enter a single letter.
 
 # Assign the input to a variable called guess.
-user_guess_letter = input("Enter a single line please: ")
-if len(user_guess_letter) == 1 and user_guess_letter.isalpha() == True:
-    print("Good guess")
-else:
-    print("Oops! That is not a valid input.")
+# user_guess_letter = input("Enter a single line please: ")
+# if len(user_guess_letter) == 1 and user_guess_letter.isalpha() == True:
+#     print("Good guess")
+# else:
+#     print("Oops! That is not a valid input.")
--- milestone_3.py
+++ milestone_3.py
@@ -1,13 +1,23 @@
 import milestone_2
 
-while True:
-
-    user_guess_letter = input("Please enter a single letter: ")
+word_list = ["Cherry", "Apricot", "Peach", "Apple", "Orange"]
+random_word = random.choice(word_list)
+print(milestone_2.random_word)
+def check_guess(guess):
+    user_guess_letter = guess.lower()
     print(user_guess_letter)
-    if user_guess_letter.isalpha():
-        if user_guess_letter.lower() in milestone_2.random_word:
-            print(f"Good guess! {user_guess_letter} is in the word")
-        else:
-            print(f"Sorry, {user_guess_letter} is not in the word. Try again.")
+    print(milestone_2.random_word)
+    if user_guess_letter in milestone_2.random_word.lower():
+        print(f"Good guess! {user_guess_letter} is in the word")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {user_guess_letter} is not in the word. Try again.")
+       
+def ask_for_input():
+    user_guess_letter = input("Please enter a single letter: ")
+    if len(user_guess_letter) == 1 and user_guess_letter.isalpha() == True:
+        check_guess(user_guess_letter)
+    else:
+         print("Invalid letter. Please, enter a single alphabetical character.")
+
+while True:
+    ask_for_input()
