031363679fb9d2e8c9fbaa2ec78130eaee973e66
robin ucar <mehmet.ucar@imediapro.co.uk>
2023-12-11 14:43:02 +0000

    refactoring variables name

--- milestone_2.py
+++ milestone_2.py
@@ -1,11 +1,11 @@
 import random
 # Five fovorite fruits
 word_list = ["Cherry", "Apricot", "Peach", "Apple", "Orange"]
-#print (word_list)
+
 # Assign the randomly generated word to a variable called word.
-word = random.choice(word_list)
-#print(word)
+random_word = random.choice(word_list)
+
 # Using the input function, ask the user to enter a single letter.
 input("Enter a single line please")
 # Assign the input to a variable called guess.
-guess = input()
+user_guess_letter = input()
